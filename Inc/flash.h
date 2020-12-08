e266703e4ed70bb377d776dab25a8c30e0869376
Nanase <nanase@users.noreply.github.com>
2020-12-09 00:58:34 +0900

    ヘッダファイルのC++対応

--- Inc/flash.h
+++ Inc/flash.h
@@ -1,21 +1,30 @@
 #ifndef __FLASH_H
 #define __FLASH_H
 
-#include "stm32f3xx_hal.h"
-
 #include <stdbool.h>
 #include <stdint.h>
 
-bool flash_erase_page(uint8_t page_number);
+#include "stm32f3xx_hal.h"
 
-bool flash_write16(uint8_t page_number, uint16_t offset, uint16_t value);
-bool flash_write32(uint8_t page_number, uint16_t offset, uint32_t value);
-bool flash_write64(uint8_t page_number, uint16_t offset, uint64_t value);
-int16_t flash_write(uint8_t page_number, uint16_t offset, const void *source, uint16_t size);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-uint16_t flash_read16(uint8_t page_number, uint16_t offset);
-uint32_t flash_read32(uint8_t page_number, uint16_t offset);
-uint64_t flash_read64(uint8_t page_number, uint16_t offset);
-void flash_read(uint8_t page_number, uint16_t offset, void *destination, uint16_t size);
+  bool flash_erase_page(uint8_t page_number);
+
+  bool flash_write16(uint8_t page_number, uint16_t offset, uint16_t value);
+  bool flash_write32(uint8_t page_number, uint16_t offset, uint32_t value);
+  bool flash_write64(uint8_t page_number, uint16_t offset, uint64_t value);
+  int16_t flash_write(uint8_t page_number, uint16_t offset, const void *source, uint16_t size);
+
+  uint16_t flash_read16(uint8_t page_number, uint16_t offset);
+  uint32_t flash_read32(uint8_t page_number, uint16_t offset);
+  uint64_t flash_read64(uint8_t page_number, uint16_t offset);
+  void flash_read(uint8_t page_number, uint16_t offset, void *destination, uint16_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
