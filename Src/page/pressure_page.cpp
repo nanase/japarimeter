ba2483d253fa5d1b8a832b18b432fd2465026c9e
Nanase <nanase@users.noreply.github.com>
2020-12-12 15:23:57 +0900

    気圧ページに二値画像を追加

--- Src/page/pressure_page.cpp
+++ Src/page/pressure_page.cpp
@@ -6,8 +6,14 @@
 extern uint32_t fixed_pressure;
 extern char buf[32];
 
+extern const uint8_t image_pressure_icon[54];
+extern const uint8_t image_pressure_value[22];
+
 void PressurePage::drawWholeScreen() {
   ssd1306_setFillMode(true);
+  ssd1306_setCursor(4, 4);
+  ssd1306_writeCompressedImageB4(image_pressure_icon, sizeof(image_pressure_icon));
+
   ssd1306_setCursor(103, 3);
   ssd1306_writeString("hPa", Font_7x10, ssd1306_white);
 
@@ -17,11 +23,25 @@ void PressurePage::drawWholeScreen() {
 }
 
 void PressurePage::draw() {
-  float pressure_hpa = fixedPressureToHectoPa(fixed_pressure);
+  float pressure_hpa        = fixedPressureToHectoPa(fixed_pressure);
+  float pressure_icon_value = pressure_hpa;
+
+  if (pressure_icon_value > 1025.0)
+    pressure_icon_value = 1025.0;
+
+  if (pressure_icon_value < 990.0)
+    pressure_icon_value = 990.0;
+
+  pressure_icon_value -= 990.0;
+
+  ssd1306_setFillMode(true);
+  ssd1306_setCursor(5, 5);
+  ssd1306_writeCompressedSlideImageB4((uint8_t)(12.0 - 12.0 * (pressure_icon_value / 35.0)), image_pressure_value,
+                                      sizeof(image_pressure_value));
 
   ssd1306_setFillMode(true);
   ssd1306_setCursor(37, 5);
-  sprintf(buf, "%04d", (uint16_t)(pressure_hpa));
+  sprintf(buf, "%4d", (uint16_t)(pressure_hpa));
   ssd1306_writeString(buf, Font_16x26, ssd1306_white);
 
   ssd1306_setFillMode(true);
