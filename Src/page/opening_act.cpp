f72937550c480b7691838e0e221d2793064b8e16
Nanase <nanase@users.noreply.github.com>
2020-12-25 22:40:06 +0900

    opening_act.cppを追加

---
+++ Src/page/opening_act.cpp
@@ -0,0 +1,20 @@
+#include "c_image.h"
+#include "fonts.h"
+#include "page.hpp"
+#include "ssd1306.h"
+
+extern const CImage image_logo;
+
+void OpeningActPage::drawWholeScreen() {
+  ssd1306_setCursor(0, 0);
+  cImage_write(&image_logo);
+}
+
+bool OpeningActPage::draw() {
+  static uint8_t count = 0;
+
+  if (count < 8)
+    count++;
+
+  return count >= 8;
+}
